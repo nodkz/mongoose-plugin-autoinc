adf49767c5a559c86a9eadb3207e037d915257a0
nodkz <pavel.chertorogov@gmail.com>
2018-07-02 19:38:26 +0600

    test: add `nextCount` test
    Related #21

---
+++ src/__tests__/github/21-test.js
@@ -0,0 +1,53 @@
+/* @flow */
+/* eslint-disable global-require */
+
+import mongoose from 'mongoose';
+import MongodbMemoryServer from 'mongodb-memory-server';
+
+let mongoServer;
+beforeAll(async () => {
+  mongoServer = new MongodbMemoryServer();
+  const mongoUrl = await mongoServer.getConnectionString();
+
+  mongoose.connect(mongoUrl, {
+    autoReconnect: true,
+    reconnectInterval: 100,
+    reconnectTries: Number.MAX_VALUE,
+  });
+});
+afterAll(() => {
+  mongoose.disconnect();
+  mongoServer.stop();
+});
+
+describe('issue #21', () => {
+  it('call model.nextCount()', done => {
+    const MySchema = new mongoose.Schema({
+      name: { type: String },
+      fruitId: Number,
+    });
+
+    const autoIncrement = require('../..');
+
+    MySchema.plugin(autoIncrement.plugin, {
+      model: 'fruit',
+      field: 'fruitId',
+    });
+
+    const MyModel = mongoose.model('Fruit', MySchema);
+
+    const a = new MyModel({
+      name: 'stone',
+    });
+
+    a.save(err => {
+      if (err) console.log('Error:', err);
+      else {
+        a.nextCount().then(count => {
+          console.log('count:', count);
+          done();
+        });
+      }
+    });
+  });
+});
